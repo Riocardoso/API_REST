eea98330ac1f9b2ab4e0d89e76e7a4de4a2cc287
Riocardoso <romario221.oficial@gmail.com>
2021-12-18 15:16:33 -0300

    adicionando dados do cliete

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 
+//enviando uma resposta para meu cliente com seu dados...
 const express = require("express");
 const app = express();
+//chamando dados do cliente
 const data = require('./data.json');
+//usando o json no express
 app.use(express.json());
 
-var verbosExpress = 0
+//pegando a requição do cliente e usando uma function como req, res..
 app.get("/cliente", function(req, res){
-
+//respondendo em forta json o dados do meu cliente
     res.json(data);
 });
-
+//inciando meu servidor...
 app.listen(3000, function(){
     console.log("serve is running");
 });
